67f0354efa533b03f2b1d13daa8852b46523b4a9
maartenwijntjes <m.w.a.wijntjes@tudelft.nl>
2019-02-06 22:58:09 +0100

    adding menu??

--- sketches/background_sketch.js
+++ sketches/background_sketch.js
@@ -1,17 +1,21 @@
 var canvas;
 var mic;
 
+var elmnt = document.getElementById("content");
+var scrollHoogte = elmnt.scrollHeight;
+var scrollBreedte = elmnt.scrollWidth;
+
 function windowResized() {
   //console.log('resized');
-  resizeCanvas(windowWidth, windowHeight);
+  resizeCanvas(windowWidth, scrollHoogte);
 }
 
 function setup() {
-  canvas = createCanvas(windowWidth, windowHeight);
+  canvas = createCanvas(windowWidth, scrollHoogte);
   canvas.position(0, 0);
-  //canvas.style('z-index', '-1');
-  // mic = new p5.AudioIn();
-  // mic.start();
+  canvas.style('z-index', '-1');
+   mic = new p5.AudioIn();
+   mic.start();
   //background(175);
 }
 
@@ -23,6 +27,6 @@ function draw() {
   if (mouseIsPressed) {
     line(pmouseX, pmouseY, mouseX, mouseY);
   }
-  // var vol = mic.getLevel();
-  // ellipse(width / 2, height / 2, vol * width);
+   var vol = mic.getLevel();
+   ellipse(width / 2, height / 2, vol * width);
 }
