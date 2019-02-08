15fea2d0280202dd2d44cdbf42e1af29a263426b
maartenwijntjes <m.w.a.wijntjes@tudelft.nl>
2019-02-08 15:54:57 +0100

    changed index

--- sketches/background_sketch.js
+++ sketches/background_sketch.js
@@ -1,8 +1,9 @@
 var canvas;
 var mic;
+var t;
 
-//var elmnt = document.getElementById("content");
-//var scrollHoogte = elmnt.scrollHeight;
+var elmnt = document.getElementById("content");
+var scrollHoogte = elmnt.scrollHeight;
 //var scrollBreedte = elmnt.scrollWidth;
 
 function windowResized() {
@@ -13,22 +14,25 @@ function windowResized() {
 function setup() {
   canvas = createCanvas(windowWidth, windowHeight);
   canvas.position(0, 0);
+  t = 0;
   //canvas.style('z-index', '-1');
   // mic = new p5.AudioIn();
   // mic.start();
   //background(175);
 }
 
-function keyPressed() {
-  clear();
-}
+
 
 function draw() {
-  if (mouseIsPressed) {
-    line(pmouseX, pmouseY, mouseX, mouseY);
-  }
+  background(255,5);
 
+  var x = width * noise(t);
+  var y = height * noise(t+5);
+  var r = 255 * noise(t+10);
+  var g = 255 * noise(t+15);
+  var b = 255 * noise(t+20);
   
-  // var vol = mic.getLevel();
-  // ellipse(width / 2, height / 2, vol * width);
+  noStroke();
+  fill(r, g, b);
+  ellipse(x, y, 120, 120);
 }
